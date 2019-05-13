85efcc8748633d672bb50017e8b7d12454f6fbe0
D. Czysz <=>
2019-05-13 15:53:57 -0500

    134 - Finished mobile

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,12 +12,21 @@ const controls = [
 
 const buildControls = (props) => (
   <div className={classes.BuildControls}>
+    <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
     {controls.map(ctrl => (
       <BuildControl
         key={ctrl.label}
         label={ctrl.label}
-        add={() => props.ingredientAdded(ctrl.type)} />
+        add={() => props.ingredientAdded(ctrl.type)}
+        remove={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]} />
     ))}
+    <button
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+      onClick={props.ordered}>
+      ORDER NOW
+    </button>
   </div>
 );
 
